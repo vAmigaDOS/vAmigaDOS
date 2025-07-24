73476f2ac7f93cabd7776c427ab915f3db28b0f5
Dirk Hoffmann <dirk.hoffmann@me.com>
2025-07-24 17:17:00 +0200

    cleanup

--- src/lib/Widgets/MainPageLink.svelte
+++ src/lib/Widgets/MainPageLink.svelte
@@ -12,12 +12,12 @@
 
 <div class="flex w-24 border-0 flex-col items-center justify-center">
 	<button
-		class="flex h-8 w-8 border-0 flex-col items-center justify-center text-gray-600"
+		class="flex h-6 w-6 border-0 flex-col items-center justify-center text-gray-600"
 		{onclick}
 	>
 		{@render icon()}
 	</button>
-	<div class="font-josefin flex border-none text-sm text-gray-600">
+	<div class="font-josefin flex border-none text-xs text-gray-600">
 		{@render description()}
 	</div>
 </div>
