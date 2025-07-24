f4866913f171768ece0b1c7cb4915d3b7babe7c4
Dirk Hoffmann <dirk.hoffmann@me.com>
2025-07-24 18:00:55 +0200

    Guru disappears when clicked

--- src/lib/Guru.svelte
+++ src/lib/Guru.svelte
@@ -2,21 +2,36 @@
 	import { wasm, enums, what, errno } from '$lib/stores';
 	$: initialized = $wasm != undefined;
 
+	let intervalId: ReturnType<typeof setInterval> | undefined = undefined;
 	let active = false;
 	let border = true;
+
 	$: if ($errno && initialized && !active) {
 		active = true;
-		setInterval(() => {
-			border = !border;
-		}, 800);
+		if (intervalId === undefined) {
+			intervalId = setInterval(() => {
+				border = !border;
+			}, 800);
+		}
 	}
 	$: borderColor = border ? 'border-red-600' : 'border-black';
+
+	function onclick() {
+		console.log('onclick');
+		$errno = 0;
+		active = false;
+		clearInterval(intervalId);
+		intervalId = undefined
+	}
 </script>
 
 {#if active}
-	<div class="relative flex justify-center bg-black text-red-600 border-[10px] {borderColor}">
+	<div
+		{onclick}
+		class="relative flex justify-center border-[10px] bg-black text-red-600 {borderColor}"
+	>
 		<div
-			class="h-24 w-full border-none flex flex-col justify-center text-lg font-azeret text-center"
+			class="font-azeret flex h-24 w-full flex-col justify-center border-none text-center text-lg"
 		>
 			<p class="p-1">
 				Emulator failure. Uncaught exception {$errno} ({$enums.ErrorCodeKey($errno)})
