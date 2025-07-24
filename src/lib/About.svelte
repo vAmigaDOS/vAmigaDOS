d019c24a63abe3587f9effa0afab4a7ff0e0ff30
Dirk Hoffmann <dirk.hoffmann@me.com>
2025-07-24 21:38:55 +0200

    cleanup

--- src/lib/About.svelte
+++ src/lib/About.svelte
@@ -14,21 +14,28 @@
 </script>
 
 <TextBox title="About" {show} {close}>
-	<h2>What is vAmigaDOS?</h2>
+	<h2>Overview</h2>
 	<p>
-		vAmigaDOS is a web-based tool designed to inspect and interact with Amiga media files such as
-		ADF and HDF disk images. It brings the classic Amiga command-line experience directly to your
-		browser, no installation required. Getting started is simple: Just drag and drop an ADF or HDF
-		file onto the browser window to launch a fully functional command shell. Once inside the shell,
-		you can navigate directories and view file contents, create and delete files and folders, or run
+		This web-based tool is designed to inspect and interact Amiga media files such as ADF and HDF
+		disk images. It brings the classic Amiga command-line experience directly to your browser, no
+		installation required. Getting started is simple: Just drag and drop an ADF or HDF file onto the
+		browser window to launch a fully functional command shell. Once inside the shell, you can
+		navigate directories and view file contents, create and delete files and folders, or run
 		built-in file system diagnostics. It's an intuitive and efficient way to explore the inner
 		structure of your Amiga disk images.
 	</p>
 	<h2>Technical Background</h2>
 	<p>
-		Technical background Behind the scenes, vAmigaDOS combines a modern Svelte frontend with the
-		power of vAmiga compiled to WebAssembly (WASM). This setup allows it to emulate the Amiga file
-		system and command interface entirely in the browser, delivering fast performance and seamless
-		usability across platforms.
+		Behind the scenes, a modern web-frontend implemented in Svelte interacts with the Amiga emulator
+		<it>vAmiga</it> compiled to WebAssembly (WASM). This setup allows it to emulate the Amiga file system
+		and command interface entirely in the browser, delivering fast performance and seamless usability
+		across platforms.
+	</p>
+	<h2>What’s Next?</h2>
+	<p>
+		While the current version offers a text-based command interface, the long-term goal is to evolve
+		it into a more visual and interactive tool. Future versions may include a graphical file
+		manager, allowing users to browse disk contents in a familiar folder view, click on files, and
+		perform common actions such as copying, renaming, or deleting.
 	</p>
 </TextBox>
