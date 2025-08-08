9117ba4d8e09ebbe9bd5fa6bb794ab785cc84f8e
Dirk Hoffmann <dirk.hoffmann@me.com>
2025-08-08 08:20:12 +0200

    Renamed emulator to Core

--- src/lib/Impressum.svelte
+++ src/lib/Impressum.svelte
@@ -11,7 +11,6 @@
 	}
 
 	let { show = $bindable(false) }: Props = $props();
-
 </script>
 
 <TextBox title="Impressum" {show} {close}>
@@ -23,6 +22,32 @@
 		D - 76133 Karlsruhe<br />
 		Germany
 	</address>
+	<h2>Datenschutz</h2>
+	Unser Hoster erhebt in sog. Logfiles folgende Daten, die Ihr Browser übermittelt: IP-Adresse,
+	die Adresse der vorher besuchten Website (Referer Anfrage-Header), Datum und Uhrzeit der Anfrage, Zeitzonendifferenz
+	zur Greenwich Mean Time, Inhalt der Anforderung, HTTP-Statuscode, übertragene Datenmenge, Website,
+	von der die Anforderung kommt und Informationen zu Browser und Betriebssystem. Das ist erforderlich,
+	um unsere Website anzuzeigen und die Stabilität und Sicherheit zu gewährleisten. Dies entspricht unserem
+	berechtigten Interesse im Sinne des Art. 6 Abs. 1 S. 1 lit. f DSGVO. Es erfolgt kein Tracking und wir
+	haben auf diese Daten keinen direkten Zugriff. Der Inhalt der auf dieser Webseite eingelesenen Datenträgerabbilder 
+	werden ausschließlich lokal im Browser gespeichert. Es gelangen keine Daten aus den Datenträgerabbildern auf den 
+	Server und es werden keine statistischen Daten erfasst.
+	
+	Wir setzen für die Zurverfügungstellung unserer Website folgenden Hoster ein: GitHub Inc. 88 Colin P Kelly Jr St San Francisco, CA 94107 United States Dieser
+	ist Empfänger Ihrer personenbezogenen Daten. Dies entspricht unserem berechtigten Interesse im Sinne
+	des Art. 6 Abs. 1 S. 1 lit. f DSGVO, selbst keinen Server in unseren Räumlichkeiten vorhalten zu müssen.
+	Serverstandort ist USA. Weitere Informationen zu Widerspruchs- und Beseitigungsmöglichkeiten gegenüber
+	GitHub finden Sie unter: https://docs.github.com/en/free-pro-team@latest/github/site-policy/github-privacy-statement#github-pages
+	Sie haben das Recht der Verarbeitung zu widersprechen. Ob der Widerspruch erfolgreich ist, ist im Rahmen
+	einer Interessenabwägung zu ermitteln. Die Daten werden gelöscht, sobald der Zweck der Verarbeitung
+	entfällt. Die Verarbeitung der unter diesem Abschnitt angegebenen Daten ist weder gesetzlich noch vertraglich
+	vorgeschrieben. Die Funktionsfähigkeit der Website ist ohne die Verarbeitung nicht gewährleistet. GitHub
+	hat Compliance-Maßnahmen für internationale Datenübermittlungen umgesetzt. Diese gelten für alle weltweiten
+	Aktivitäten, bei denen GitHub personenbezogene Daten von natürlichen Personen in der EU verarbeitet.
+	Diese Maßnahmen basieren auf den EU-Standardvertragsklauseln (SCCs). Weitere Informationen finden Sie
+	unter: https://docs.github.com/en/free-pro-team@latest/github/site-policy/github-data-protection-addendum#attachment-1–the-standard-contractual-clauses-processors
+
+	<!--
 	<h2>Datenschutz</h2>
 	<p>
 		Durch die Nutzung unserer Website erklären Sie sich mit der Erhebung, Verarbeitung und Nutzung
@@ -43,4 +68,5 @@
 			>Datenschutzerklärung von GitHub</a
 		>.
 	</p>
+	-->
 </TextBox>
